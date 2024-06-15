9f6fa4a5f92144651bcab4402edf2db36ef67234
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-15 23:18:29 +0100

    feat: changes

--- modules/misc/misc.controllers.ts
+++ modules/misc/misc.controllers.ts
@@ -16,10 +16,16 @@ export function getNftItemMetadata(req: Request, res: Response) {
     const { username } = req.params;
 
     const obj = {
-        name: `@${username}`,
+        name: username,
         description:
             'A part of Quyx username - the starting point of your decentralized identity management',
         image: `https://media.quyx.xyz/nft/${username}`,
+        buttons: [
+            {
+                label: 'Go to Quyx',
+                uri: 'https://quyx.xyz',
+            },
+        ],
         attributes: [
             {
                 trait_type: 'Length',
