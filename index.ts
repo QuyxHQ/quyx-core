f2cdde5946780b8be8ed33e893ffcb10c3ab0978
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-28 07:01:42 +0100

    trigger

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ const userRepo = new UserRepo();
     try {
         const app = createServer();
         const httpServer = http.createServer(app);
-        const PORT = 3005;
+        const PORT = 3010;
 
         const io = new Server(httpServer, {
             cors: {
